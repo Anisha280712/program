88fbec68b62d9860ab6e0de19a8affc367d10dff
Anisha280712 <Sivakumar.Anitha.T@gmail.com>
2025-11-05 18:14:04 +0000

    percentage.py

---
+++ percentage.py
@@ -0,0 +1,8 @@
+print("enter the marks of four subjects")
+maths=int(input("math:"))
+science=int(input("science:"))
+english=int(input("english:"))
+french=int(input("french:"))
+sum=maths+science+english+french
+percentage=(sum/400)*100
+print("percentage marks=",percentage)
