0b36cbb531ca3f3032bba9aa59991c0210ace5bf
Anisha280712 <Sivakumar.Anitha.T@gmail.com>
2025-11-05 18:43:58 +0000

    storing birthday.py

---
+++ storing birthday.py
@@ -0,0 +1,16 @@
+name="raj"
+birthday="1 febuary"
+name="lara"
+birthday="15 septenber"
+name="prineetha"
+birthday="23 november"
+name="mark"
+birthday="27 july"
+name="jacob"
+birthday="30 january"
+print("raj:1 febuary")
+print("lara:15 setember")
+print("prineetha:23 november")
+print("mark:27 july")
+print("jacob:30 january")
+print ("happy birthday !")
