2502286240b1bdad0420df9583798dd68dc960aa
Anisha280712 <Sivakumar.Anitha.T@gmail.com>
2025-11-12 17:23:51 +0000

    summer times.py

---
+++ summer times.py
@@ -0,0 +1,5 @@
+temprature = float(input("Enter the temprature in degrees Celsius:"))
+if temprature>= 20:
+    print("it is warm ! you can wear light and soft clothes")
+else: 
+    print("its too cold ! Better wear a jacket or jumper")
