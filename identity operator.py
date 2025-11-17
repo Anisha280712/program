4485cc0250d8c6c70b8912b8b475d4b84ae88b1d
Anisha280712 <Sivakumar.Anitha.T@gmail.com>
2025-11-17 17:50:01 +0000

    indentity operator .py

---
+++ identity operator.py
@@ -0,0 +1,20 @@
+x=6
+if(type(x)is int):
+    print("true")
+else:
+    print("false")
+
+y=7.4
+if(type(y)is not float):
+    print("true")
+else:
+    print("false")
+
+a=20
+b=20
+if(a is b):
+    print("x and y are the same")
+
+c=30
+if(a is not c):
+    print("x and y are diffrent")
