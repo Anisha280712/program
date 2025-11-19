5d180f19c619cd92333ac55faef8f1b35c1c0800
Anisha280712 <Sivakumar.Anitha.T@gmail.com>
2025-11-19 17:55:36 +0000

    divisable.py

---
+++ divisable.py
@@ -0,0 +1,6 @@
+number1=int(input( "enter the first number :"))
+number2=int(input("enter the first number :"))
+if number1 % number2==0 : 
+    print(number1 , "is divisable by",number2)
+else:
+    print(number1 , "is not divisable by",number2)
