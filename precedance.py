0b1adf795f523d4e2a9439d415d540669f15f946
Anisha280712 <Sivakumar.Anitha.T@gmail.com>
2025-11-19 17:48:48 +0000

    precendance.py

---
+++ precedance.py
@@ -0,0 +1,14 @@
+v=5
+w=7
+x=6
+y=8
+z=0
+z=(v+w)*y/x; 
+print("value of (v+w)*y/x:",z)
+
+name="alex"
+age=0
+if name=="alex" and name=="jhon" or age>=0:
+    print("hello")
+else:
+    print("goodbye")
