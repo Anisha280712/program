8b6299c5114469c87015588bc67dd598e7cb13b2
Anisha280712 <Sivakumar.Anitha.T@gmail.com>
2025-11-26 18:06:43 +0000

    commit.

---
+++ reverse order.py
@@ -0,0 +1,7 @@
+n=int(input("enter the value of n:"))
+print("number from {0} to {1} are :".format(n,1))
+print("number from ", n, "to 1 are :" )
+for i in range (n,0,-1):
+    print(i)
+for i in range(1,n+1):
+    print(i)
